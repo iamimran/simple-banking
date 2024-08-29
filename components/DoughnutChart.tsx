2ef694e8e7e4ae2ad56b700b100f60ed6c2dbaba
Imran Ahmed <iam.imran@yahoo.com>
2024-08-29 16:34:54 +0500

    feat: Add HeaderBox and TotalBalanceBox components
    This commit adds the HeaderBox and TotalBalanceBox components to the project. The HeaderBox component is responsible for rendering a header with a title, user greeting, and subtext. The TotalBalanceBox component displays the total number of bank accounts and the total current balance. It also includes a DoughnutChart component to visualize the distribution of funds across different banks.

---
+++ components/DoughnutChart.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { ArcElement, Chart as ChartJS, Legend, Tooltip } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+ChartJS.register(ArcElement, Legend, Tooltip);
+
+const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
+  const data = {
+    datasets: [
+      {
+        label: "Banks",
+        data: [1250, 2500, 3750],
+        backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+      },
+    ],
+    labels: ["Bank 1", "Bank 2", "Bank 3"],
+  };
+  return (
+    <Doughnut
+      data={data}
+      options={{ cutout: "60%", plugins: { legend: { display: false } } }}
+    />
+  );
+};
+
+export default DoughnutChart;
